c153c2e8b5b3dbc928055ebf2704be4012753e1d
christmex <christmex@yahoo.com>
2023-07-26 15:29:27 +0700

    Modify Menu item

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -1,10 +1,17 @@
 {{-- This file is used for menu items by any Backpack v6 theme --}}
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 
-<x-backpack::menu-item title="Authors" icon="la la-question" :link="backpack_url('author')" />
-<x-backpack::menu-item title="Book types" icon="la la-question" :link="backpack_url('book-type')" />
-<x-backpack::menu-item title="Publishers" icon="la la-question" :link="backpack_url('publisher')" />
-<x-backpack::menu-item title="Book locations" icon="la la-question" :link="backpack_url('book-location')" />
+
+<x-backpack::menu-dropdown title="Book" icon="la la-book">
+    <x-backpack::menu-dropdown-header title="Book Settings" />
+    <x-backpack::menu-dropdown-item title="Authors" icon="la la-question" :link="backpack_url('author')" />
+    <x-backpack::menu-dropdown-item title="Book types" icon="la la-question" :link="backpack_url('book-type')" />
+    <x-backpack::menu-dropdown-item title="Publishers" icon="la la-question" :link="backpack_url('publisher')" />
+    <x-backpack::menu-dropdown-item title="Book locations" icon="la la-question" :link="backpack_url('book-location')" />
+
+    <x-backpack::menu-dropdown-header title="Book" />
+    <x-backpack::menu-dropdown-item title="Books" icon="la la-question" :link="backpack_url('book')" />
+</x-backpack::menu-dropdown>
+
 <x-backpack::menu-item title="Departments" icon="la la-question" :link="backpack_url('department')" />
 <x-backpack::menu-item title="Members" icon="la la-question" :link="backpack_url('member')" />
-<x-backpack::menu-item title="Books" icon="la la-question" :link="backpack_url('book')" />
