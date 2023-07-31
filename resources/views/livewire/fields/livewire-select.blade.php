7795d428725d8f8e17156116b9bbcc511480b151
christmex <christmex@yahoo.com>
2023-07-31 14:31:02 +0700

    fix ui

--- resources/views/livewire/fields/livewire-select.blade.php
+++ resources/views/livewire/fields/livewire-select.blade.php
@@ -8,7 +8,7 @@
             @include('crud::fields.inc.attributes') 
         >
         @if(!empty($form_search) && !empty($search_result))
-            <ul class="list-group" style="z-index:999; width:100%; box-shadow:0 1px 2px 0 rgba(0, 0, 0, 0.05)">
+            <ul class="list-group position-absolute bg-white" style="z-index:999; width:100%; box-shadow:0 1px 2px 0 rgba(0, 0, 0, 0.05)">
                 @if(!empty($search_result))
                     @foreach($search_result as $item)
                         <li class="list-group-item"style="cursor:pointer" wire:click="setValue({{$item->id}},'{{ $item->{$field["attribute"]} }}')">{{ $item->{$field['attribute']} }}</li>
